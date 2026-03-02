5a8542a776efc03397f623dc6178e48d2397106b
Hammad Lodhi <hammadak05@gmail.com>
2026-03-02 14:03:57 +0500

    update agency password

--- src/agencies/dto/update-agency.dto.ts
+++ src/agencies/dto/update-agency.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, OmitType } from '@nestjs/swagger';
 import { CreateAgencyDto } from './create-agency.dto';
 
-export class UpdateAgencyDto extends PartialType(CreateAgencyDto) {}
+export class UpdateAgencyDto extends PartialType(
+  OmitType(CreateAgencyDto, ['password'] as const),
+) {}
